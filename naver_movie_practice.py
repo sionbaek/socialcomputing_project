2f3afd24f76e3465a4b48193a18fda35c3690199
Sungmin <qkrtjdals098@gmail.com>
2018-12-10 14:55:12 +0900

    naver movie 40

---
+++ naver_movie_practice.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from time import sleep
+import requests
+import json
+import re
+from selenium.common.exceptions import NoSuchElementException
+import csv
+import os
+
+
+#WINDOWS OS
+header = {'User-Agent': ''}
+
+options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+# options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
+# options.add_argument('--incognito')
+
+d = webdriver.Chrome(executable_path='./chromedriver.exe', chrome_options=options)
+# d=webdriver.Chrome(executable_path='./chromedriver', chrome_options=options) #MAC OS인 경우 이걸로 하세요, 윗줄 주석 처리
+# d = webdriver.Chrome(executable_path='./chromedriver.exe')
+d.implicitly_wait(3)
+
+with open('./movie_genre_korea.csv', 'r') as csvfile:
+    list_reader=csv.reader(csvfile, delimiter=',')
+    movie_list=list(list_reader)
+
+d.get('https://movie.naver.com/')
+
+for movie in movie_list:
+    movie_input=d.find_element_by_xpath('//*[@id="ipt_tx_srch"]')
+    d.execute_script('''
+        var movie_input=arguments[0];
+        var value=arguments[1];
+        movie_input.value=value;
+        ''', movie_input, movie[0])
+    d.find_element_by_xpath('//*[@class="btn_srch"]').click()
+    sleep(2)
+    d.find_element_by_xpath('//*[@class="search_list_1"][1]/li[1]/p/a').click()
+    sleep(3)
+
+
+# d.executeScript("d.find_element_by_xpath('//*[@id=\"ipt_tx_srch\"]').setAttribute('value', 'new value for element')")
--- naver_movie_review.py
+++ naver_movie_review.py
@@ -15,7 +15,7 @@ def get_page_num():
 
     return page_num
 
-def get_reviews(page_num, released):
+def get_reviews(page_num, released, genre):
     result=list()
     for j in range(1, page_num+1):
         sleep(2)
@@ -32,7 +32,7 @@ def get_reviews(page_num, released):
                 review=d.find_element_by_xpath(review_xpath).text
                 re_time=d.find_element_by_xpath(time_xpath).text
                 print("score: {}, review: {}, time:{}".format(score, review, re_time))
-                result.append({'score':score, 'review':review, 'time':re_time, 'released':released})
+                result.append({'score':score, 'review':review, 'time':re_time, 'released':released, 'genre':genre})
             except:
                 print("no xpath found")
                 pass
@@ -43,41 +43,69 @@ def get_reviews(page_num, released):
 header = {'User-Agent': ''}
 
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
-options.add_argument('--incognito')
+# options.add_argument('headless')
+# options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36")
+# options.add_argument('--incognito')
 
 d = webdriver.Chrome(executable_path='./chromedriver.exe', chrome_options=options)
 # d=webdriver.Chrome(executable_path='./chromedriver', chrome_options=options) #MAC OS인 경우 이걸로 하세요, 윗줄 주석 처리
 # d = webdriver.Chrome(executable_path='./chromedriver.exe')
 d.implicitly_wait(3)
 
-d.get('https://movie.naver.com/movie/bi/mi/basic.nhn?code=167638') #167638
-d.find_element_by_xpath('//*[@class="tab05_off"]').click()
+with open('./movie_genre_korea.csv', 'r') as csvfile:
+    list_reader=csv.reader(csvfile, delimiter=',')
+    movie_list=list(list_reader)
 
-movie_title=d.find_element_by_xpath('//*[@class="mv_info_area"]/div[@class="mv_info"]/h3/a[1]').text
-print(movie_title)
-movie_title.replace(":", "_")
+d.get('https://movie.naver.com/')
 
-# 개봉 후 평점 받아오기, 한 페이지에 열 개
-# page 내부, 네티즌 댓글 전용 iframe 들어가기
-d.switch_to.frame("pointAfterListIframe")
-page_num=get_page_num()
-result=get_reviews(page_num, True)
+for movie in movie_list:
+    # title=movie[0]
+    title="거미줄에 걸린 소녀"
+    genre=movie[1]
 
-#개봉 전 받아오기
-d.switch_to.default_content()
-d.find_element_by_xpath('//*[@id="beforePointTab"]/a').click()
+    movie_input=d.find_element_by_xpath('//*[@id="ipt_tx_srch"]')
+    d.execute_script('''
+        var movie_input=arguments[0];
+        var value=arguments[1];
+        movie_input.value=value;
+        ''', movie_input, title)
+    d.find_element_by_xpath('//*[@class="btn_srch"]').click()
+    sleep(2)
+    d.find_element_by_xpath('//*[@class="search_list_1"][1]/li[1]/p/a').click()
+    sleep(3)
 
-d.switch_to.frame("pointAfterListIframe")
-page_num2=get_page_num()
-result2=get_reviews(page_num2, False)
+    # 리뷰 페이지로 이동
+    d.find_element_by_xpath('//*[@class="tab05_off"]').click()
+
+    movie_title=d.find_element_by_xpath('//*[@class="mv_info_area"]/div[@class="mv_info"]/h3/a[1]').text
+    print(movie_title)
+    movie_title.replace(":", "_")
+
+    # 개봉 후 평점 받아오기, 한 페이지에 열 개
+    # page 내부, 네티즌 댓글 전용 iframe 들어가기
+    d.switch_to.frame("pointAfterListIframe")
+    page_num=get_page_num()
+    result=get_reviews(page_num, True, genre)
+
+    #개봉 전 받아오기
+    d.switch_to.default_content()
+    d.find_element_by_xpath('//*[@id="beforePointTab"]/a').click()
+
+    d.switch_to.frame("pointAfterListIframe")
+    page_num2=get_page_num()
+    result2=get_reviews(page_num2, False, genre)
+
+    # 기본으로 되돌리기
+    d.switch_to.default_content()
+
+# d.get('https://movie.naver.com/movie/bi/mi/basic.nhn?code=167638') #167638
+
 
 d.quit()
 
 # csv로 저장
 with open('./review_{}.csv'.format(movie_title), 'w', encoding='utf8') as csvfile:
-    fieldnames=["score", "review", "time", "released"]
+    fieldnames=["score", "review", "time", "released", "genre"]
     writer=csv.DictWriter(csvfile, fieldnames=fieldnames)
 
     writer.writeheader()
